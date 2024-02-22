84824caf52c009f5b0ea7e9c999187d419fdddfc
Relevie Abalahon <s.abalahon.relevie@gmail.com>
2024-02-22 19:39:34 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:activity2/userdetails_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainPage());
+}
+
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String username;
+  final String street;
+  final String suite;
+  final String city;
+  final String zipcode;
+  final String lat;
+  final String lng;
+  final String phone;
+  final String website;
+  final String companyname;
+  final String catchphrase;
+  final String bs;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+    required this.lat,
+    required this.lng,
+    required this.phone,
+    required this.website,
+    required this.companyname,
+    required this.catchphrase,
+    required this.bs,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      username: json['username'],
+      street: json['address']['street'],
+      suite: json['address']['suite'],
+      city: json['address']['city'],
+      zipcode: json['address']['zipcode'],
+      lat: json['address']['geo']['lat'],
+      lng: json['address']['geo']['lng'],
+      phone: json['phone'],
+      website: json['website'],
+      companyname: json['company']['name'],
+      catchphrase: json['company']['catchPhrase'],
+      bs: json['company']['bs'],
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'User List App',
+      home: UserListScreen(),
+    );
+  }
+}
+
+class UserListScreen extends StatefulWidget {
+  const UserListScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserListScreenState createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  late Future<List<User>> users;
+
+  @override
+  void initState() {
+    super.initState();
+    users = fetchUsers();
+  }
+
+  Future<List<User>> fetchUsers() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'User List',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.blue, fontSize: 25.0),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: FutureBuilder<List<User>>(
+          future: users,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  User currentUser = snapshot.data![index];
+
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        left: 18.0, right: 18.0, bottom: 5.0),
+                    child: Container(
+                      margin: const EdgeInsets.all(2.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: const Color.fromARGB(71, 158, 158, 158),
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: ListTile(
+                        leading: const Icon(
+                          Icons.account_circle_sharp,
+                          size: 40.0,
+                          color: Colors.grey,
+                        ),
+                        title: Text(
+                          currentUser.name,
+                          style: const TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          currentUser.email,
+                          style: const TextStyle(
+                              fontSize: 12.0, fontWeight: FontWeight.w600),
+                        ),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.info, color: Colors.blue),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    UserDetailsScreen(user: currentUser),
+                              ),
+                            );
+                          },
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/userdetails_screen.dart
@@ -0,0 +1,354 @@
+import 'package:activity2/main.dart';
+import 'package:flutter/material.dart';
+
+class UserDetailsScreen extends StatelessWidget {
+  final User user;
+
+  const UserDetailsScreen({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.blue.shade50,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 25.0, left: 16.0, right: 16.0, bottom: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //  Header
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.chevron_left_sharp),
+                    ),
+                    const Text(
+                      'User Details',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 15.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: const Color.fromARGB(71, 158, 158, 158),
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.account_circle_sharp,
+                          color: Colors.blue,
+                          size: 100.0,
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          user.name,
+                          style: const TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Email: ${user.email}',
+                          style: const TextStyle(
+                              fontSize: 12.0, fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //Starting of the Users Details
+                const SizedBox(height: 10.0),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 180,
+                          height: 160,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: const Color.fromARGB(71, 158, 158, 158),
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          //users info
+                          child: Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Center(
+                                    child: Text(
+                                      'Account Details',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 15.0,
+                                        color: Color.fromARGB(255, 3, 17, 95),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    'ID No. : ${user.id}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Username : ${user.username}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        // Address Container
+                        const SizedBox(width: 5),
+                        Container(
+                          width: 175,
+                          height: 160,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: const Color.fromARGB(71, 158, 158, 158),
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Center(
+                                    child: Text(
+                                      'Address Details',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 15.0,
+                                        color: Color.fromARGB(255, 3, 17, 95),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Street: ${user.street}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Suite: ${user.suite}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'City: ${user.city}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Zipcode: ${user.zipcode}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 10.0), // Add spacing between rows
+                    Row(
+                      children: [
+                        Container(
+                          height: 160,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: const Color.fromARGB(71, 158, 158, 158),
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          //Geo Container
+                          child: Container(
+                            height: 170,
+                            width: 175,
+                            margin: const EdgeInsets.all(15.0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Center(
+                                    child: Text(
+                                      'Geography Details',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 15.0,
+                                        color: Color.fromARGB(255, 3, 17, 95),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Latitude: ${user.lat}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Longitude: ${user.lng}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        // Contact Info Container
+                        const SizedBox(width: 5),
+                        Container(
+                          width: 175,
+                          height: 170,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: const Color.fromARGB(71, 158, 158, 158),
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Center(
+                                    child: Text(
+                                      'Contact Details',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 15.0,
+                                        color: Color.fromARGB(255, 3, 17, 95),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Phone: ${user.phone}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Website: ${user.website}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 10.0), // Add spacing between rows
+                    Row(
+                      children: [
+                        Container(
+                          width: 358,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: const Color.fromARGB(71, 158, 158, 158),
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          //Company Container
+                          child: Container(
+                            margin: const EdgeInsets.all(15.0),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Center(
+                                    child: Text(
+                                      'Company Details',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 15.0,
+                                        color: Color.fromARGB(255, 3, 17, 95),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Name: ${user.companyname}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Catch Phrase: ${user.catchphrase}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    'Business: ${user.bs}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
